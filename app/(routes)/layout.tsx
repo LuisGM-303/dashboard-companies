5d00827d6416b34677facaa25c53f0dc39ab1eda
Luis Francisco García Malagón <lgarciamalagan@gmail.com>
2024-07-07 11:25:56 -0600

    Sidebar Completed

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -1,4 +1,5 @@
 import { Navbar } from "@/components/Navbar/Navbar";
+import { Sidebar } from "@/components/Sidebar";
 import React from "react";
 
 export default function LayoutDashboard({
@@ -8,7 +9,9 @@ export default function LayoutDashboard({
 }) {
   return (
     <div className="flex w-full h-full">
-      <div className="hidden xl:block w-80 h-full xl:fixed">Sidebar</div>
+      <div className="hidden xl:block w-80 h-full xl:fixed">
+        <Sidebar />
+      </div>
       <div className="w-full xl:ml-80">
         <Navbar />
         <div className="p-4 bg-[#fafbfc] dark:bg-secondary">{children}</div>
