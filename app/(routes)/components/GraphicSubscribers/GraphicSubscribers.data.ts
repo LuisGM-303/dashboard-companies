8b811d74fe34ca24929ee88c4b1155adc358b8bc
Luis Francisco García Malagón <lgarciamalagan@gmail.com>
2024-07-10 19:19:06 -0600

    New sections added

---
+++ app/(routes)/components/GraphicSubscribers/GraphicSubscribers.data.ts
@@ -0,0 +1,32 @@
+export const dataGraphics = [
+  {
+    year: "2016",
+    newCustomers: 4000,
+    oldCustomers: 2000,
+  },
+  {
+    year: "2017",
+    newCustomers: 4600,
+    oldCustomers: 1000,
+  },
+  {
+    year: "2018",
+    newCustomers: 2000,
+    oldCustomers: 9800,
+  },
+  {
+    year: "2019",
+    newCustomers: 2780,
+    oldCustomers: 3908,
+  },
+  {
+    year: "2020",
+    newCustomers: 1890,
+    oldCustomers: 1000,
+  },
+  {
+    year: "2021",
+    newCustomers: 4000,
+    oldCustomers: 3800,
+  },
+];
